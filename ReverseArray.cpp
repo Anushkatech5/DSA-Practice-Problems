6c5422855d613bc64b47f44ee34e8cad3f56fced
Anushka <123286374+Anushkatech5@users.noreply.github.com>
2023-09-26 13:55:47 +0530

    ReverseArray.cpp

---
+++ ReverseArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+  int arr[]={10,20,30,40,50,60,70,80};
+  int size=8;
+
+  int start=0;
+  int end= size-1;
+
+  while(start<=end){
+    //step 1:
+    swap(arr[start],arr[end]);
+    //step 2:
+    start++;
+    //step 3:
+    end--;
+  }
+  //printing array
+  for(int i=0;i<size;i++)
+    cout<< arr[i]<<" ";
+}
