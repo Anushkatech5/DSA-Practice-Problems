36c2e0da396b9ed242614e8a1304d66b118b2478
Anushka <123286374+Anushkatech5@users.noreply.github.com>
2024-01-08 23:56:00 +0530

    Create L9.java
    Add palindrome number

---
+++ L9.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public boolean isPalindrome(int x) {
+
+        int sum = 0;
+        int target = x;
+
+        while(x>0) {
+            int temp = x % 10;
+            x = x/10;
+            sum = sum * 10 +temp;
+        }
+        return sum == target;
+    }
+}
