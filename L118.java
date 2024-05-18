db83010ad0e2cfb0ca288f21b5d4b43f840b2216
Anushka <guptaanushka024@gmail.com>
2024-05-19 01:36:18 +0530

    L118.java

---
+++ L118.java
@@ -0,0 +1,25 @@
+//PASCAL'S TRIANGLE
+
+class Solution {
+
+    public static List<Integer> generateRow(int row)
+    {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int row = 1; row <= numRows; row++) {
+            ans.add(generateRow(row));
+        }
+    return ans;
+    }
+}
