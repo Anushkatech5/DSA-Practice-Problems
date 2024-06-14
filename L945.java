ef93964421a905c83b467da3db3bfcfe9e0ff104
Anushka <guptaanushka024@gmail.com>
2024-06-15 00:39:07 +0530

    L945.java
    Example 1:
    
    Input: nums = [1,2,2]
    Output: 1
    Explanation: After 1 move, the array could be [1, 2, 3].
    Example 2:
    
    Input: nums = [3,2,1,2,1,7]
    Output: 6
    Explanation: After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
    It can be shown with 5 or less moves that it is impossible for the array to have all unique values.

---
+++ L945.java
@@ -0,0 +1,15 @@
+//Minimum Increment to Make Array Unique
+
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for( int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i-1]) {
+                ans += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        return ans;
+    }
+}
