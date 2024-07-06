f32d1468165b567a09ad03db881439148df7765b
Anushka <guptaanushka024@gmail.com>
2024-07-07 00:29:47 +0530

    L2582.java
    class Solution {
        public int passThePillow(int n, int time) {
            int chunks = time / (n - 1);
            return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
        }
    }

---
+++ L2582.java
@@ -0,0 +1,8 @@
+//Pass the Pillow
+
+class Solution {
+    public int passThePillow(int n, int time) {
+        int chunks = time / (n - 1);
+        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+    }
+}
