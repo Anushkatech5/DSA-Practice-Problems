06b7ab7192c9422d355538984beb8fab2e10b68d
Anushka <guptaanushka024@gmail.com>
2024-07-29 00:11:16 +0530

    L328.java
    Example 1:
    //image
    
    Input: head = [1,2,3,4,5]
    Output: [1,3,5,2,4]
    
    Example 2:
    //image
    
    Input: head = [2,1,3,5,6,4,7]
    Output: [2,3,6,7,1,5,4]

---
+++ L328.java
@@ -0,0 +1,34 @@
+//Odd Even Linked List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenListNodeHead = even; 
+        // to store the first element of even, as it will change later, but will be needed at the end to add at the next of odd.
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenListNodeHead;
+
+        return head;
+    }
+}
