0e0ae417aa103a218ae30521e681e8c33866b0a3
Anushka <guptaanushka024@gmail.com>
2024-07-29 19:33:26 +0530

    L264.java
    Example 1:
    
    Input: n = 10
    Output: 12
    Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 ugly numbers.
    
    Example 2:
    
    Input: n = 1
    Output: 1
    Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.

---
+++ L264.java
@@ -0,0 +1,30 @@
+//Uglhy Number II
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
+
+        int index2 = 0, index3 = 0, index5 = 0;
+        int nextOf2 = 2, nextOf3 = 3, nextOf5 = 5;
+
+        for (int i = 1; i < n; i++) {
+            int nextUgly = Math.min(Math.min(nextOf2, nextOf3), nextOf5);
+            uglyNumbers[i] = nextUgly;
+
+            if (nextUgly == nextOf2) {
+                index2++;
+                nextOf2 = uglyNumbers[index2] * 2;
+            }
+            if (nextUgly == nextOf3) {
+                index3++;
+                nextOf3 = uglyNumbers[index3] * 3;
+            }
+            if (nextUgly == nextOf5) {
+                index5++;
+                nextOf5 = uglyNumbers[index5] * 5;
+            }
+        }
+        return uglyNumbers[n - 1];
+    }
+}
